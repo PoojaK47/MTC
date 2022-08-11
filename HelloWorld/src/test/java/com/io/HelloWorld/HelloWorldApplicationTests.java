b467fe249b0d295f48fa81e996d0641a1bfc82dd
PoojaK47 <72204131+PoojaK47@users.noreply.github.com>
2022-08-11 15:11:18 +0530

    Add files via upload

---
+++ HelloWorld/src/test/java/com/io/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.io.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	public void sayHello() {
+		System.out.println("Say Hi!!!");
+	}
+
+}
